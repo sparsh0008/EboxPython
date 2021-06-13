23b9e45a1be042fcadf1c09083a25ab2c393da27
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2021-06-13 21:58:58 +0530

    Create main.py

---
+++ DisplayDetails/main.py
@@ -0,0 +1,10 @@
+from Item import *
+
+iid = input()
+item_name = input()
+company_name = input()
+price = int(input())
+# Fill your code here
+
+obj1 = Item(iid, item_name, company_name, price)
+obj1.display()
